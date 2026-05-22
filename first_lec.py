dff906b4eb1704e84068eef73e2a31697bb08556
Swarup Mandhare <swarup.mandhare9822@gmail.com>
2026-05-22 19:56:54 +0530

    first commit

---
+++ first_lec.py
@@ -0,0 +1,5 @@
+print("おはよう")
+name = "swarup"
+age = -21
+price = 2607.04
+print(age)
---
+++ result.py
@@ -0,0 +1,3 @@
+print("Welcome to the result dashboard")
+name = str(input(print("Please enter your name")))
+print("hello", name)
